8e341a54e5643f4a869a84e1cc3d4bdc0e8e4514
Duc Truong <phuduc1193@gmail.com>
2018-03-13 19:50:44 +0700

    Add Identity Server

---
+++ IdentityServer/ClientExtension.cs
@@ -0,0 +1,13 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class ClientExtension : Client
+    {
+        public string APISecret { get; set; }
+    }
+}
---
+++ IdentityServer/Config.cs
@@ -0,0 +1,53 @@
+﻿using IdentityServer4.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            var listResources = LoadApiResources();
+            return listResources.Select(x => new ApiResource(x.Name, x.DisplayName));
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            var listClients = LoadClients();
+            return listClients.Select(x => new Client
+            {
+                ClientId = x.ClientId,
+                AllowedGrantTypes = x.AllowedGrantTypes,
+                ClientSecrets = { new Secret(x.APISecret.Sha256()) },
+                AllowedScopes = x.AllowedScopes
+            });
+        }
+
+        private static List<ApiResource> LoadApiResources()
+        {
+            var path = Directory.GetCurrentDirectory() + @"\Settings\apiResources.json";
+            using (StreamReader r = new StreamReader(path))
+            {
+                string json = r.ReadToEnd();
+                var result = JsonConvert.DeserializeObject<List<ApiResource>>(json);
+                return result;
+            }
+        }
+
+        private static List<ClientExtension> LoadClients()
+        {
+            var path = Directory.GetCurrentDirectory() + @"\Settings\clients.json";
+            using (StreamReader r = new StreamReader(path))
+            {
+                string json = r.ReadToEnd();
+                var result = JsonConvert.DeserializeObject<List<ClientExtension>>(json);
+                return result;
+            }
+        }
+    }
+}
